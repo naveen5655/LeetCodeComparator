75e9d1012f965b9078a78ee06bb8e2fedc771a4d
Badisa Naveen <naveenbadisa5655@gmail.com>
2024-05-25 12:36:58 +0530

    first commit

---
+++ src/Component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+function Footer(){
+    return (
+      <footer>
+        <div className='footer'>
+          Copyright &copy; 2024-2025 All Rights Reserved.
+        </div> 
+      </footer>       
+    );
+  }
+ 
+export default Footer;
